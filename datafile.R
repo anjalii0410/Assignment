d13043336aa920d4236fa685e0fc2fea34d7fd6d
anjalii0410 <104578585+anjalii0410@users.noreply.github.com>
2022-04-28 20:18:10 +0530

    Add files via upload

---
+++ datafile.R
@@ -0,0 +1,6 @@
+setwd("E:/WCC 2020-2023/Semester-4/Statistical Methods")
+read.csv("msme_scheme.csv")
+my.xldataset <- read.csv("msme_scheme.csv")
+mean(my.xldataset$TOTAL)
+var(my.xldataset$TOTAL)
+
