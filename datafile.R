fc09615d963e9bd032c3dcc1197db015e9546597
anjalii0410 <104578585+anjalii0410@users.noreply.github.com>
2022-04-29 20:05:23 +0530

    Delete datafile.R

--- datafile.R
+++
@@ -1,6 +0,0 @@
-setwd("E:/WCC 2020-2023/Semester-4/Statistical Methods")
-read.csv("msme_scheme.csv")
-my.xldataset <- read.csv("msme_scheme.csv")
-mean(my.xldataset$TOTAL)
-var(my.xldataset$TOTAL)
-
